1b056e32eec9e82ae5058c585f60a9680efae060
Konekoya <tank158r5155@yahoo.com.tw>
2018-03-05 06:40:05 +0800

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  verbose: true,
+  collectCoverageFrom: [
+    'src/**/*.{js}',
+    '!**/node_modules/**',
+    '!**/.cache/**',
+  ],
+};
---
+++ src/layouts/Conteainer.test.js
@@ -0,0 +1,5 @@
+describe('describes your test here!', () => {
+  test('expect ture to be true', () => {
+    expect(true).toBe(true);
+  });
+});
